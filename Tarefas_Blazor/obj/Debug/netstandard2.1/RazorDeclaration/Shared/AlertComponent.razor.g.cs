3735e7ddb725bbcfd9546edcb497cbbc5e92c3a3
BRENO_MORAIS <brenofranciscomorais@gmail.com>
2021-04-10 11:37:43 -0400

    Correção do Blazor
    Foi feito a correção de rotas do blazor.

--- Tarefas_Blazor/obj/Debug/netstandard2.1/RazorDeclaration/Shared/AlertComponent.razor.g.cs
+++ Tarefas_Blazor/obj/Debug/netstandard2.1/RazorDeclaration/Shared/AlertComponent.razor.g.cs
@@ -13,70 +13,70 @@ namespace Tarefas_Blazor.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 1 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 2 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 3 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 4 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 5 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 6 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 7 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 8 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Tarefas_Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 9 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Tarefas_Blazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
+#line 11 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\_Imports.razor"
 using Tarefas_Blazor.Entidades;
 
 #line default
@@ -90,7 +90,7 @@ using Tarefas_Blazor.Entidades;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 8 "E:\_DEV BRENO-MORAIS\_Código-Fonte\Tarefa_Blazor\Tarefas_Blazor\Shared\AlertComponent.razor"
+#line 8 "D:\_DESENVOLVIMENTO\CODIGO FONTE\Tarefa_Blazor\Tarefas_Blazor\Shared\AlertComponent.razor"
        
     [Parameter]
     public string Menssagem { get; set; }
